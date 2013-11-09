9fbbed34bb4f0dbf58e1fdab380ba5523ed141c9
simonbromberg <simon@encoretheapp.com>
2013-11-09 16:01:47 -0500

    added body view controller

---
+++ SOSBaycrest/SOSBaycrest/SOSBodyViewController.h
@@ -0,0 +1,13 @@
+//
+//  SOSBodyViewController.h
+//  SOSBaycrest
+//
+//  Created by Simon Bromberg on 11/9/2013.
+//  Copyright (c) 2013 Michael Matan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SOSBodyViewController : UIViewController
+
+@end
---
+++ SOSBaycrest/SOSBaycrest/SOSBodyViewController.m
@@ -0,0 +1,38 @@
+//
+//  SOSBodyViewController.m
+//  SOSBaycrest
+//
+//  Created by Simon Bromberg on 11/9/2013.
+//  Copyright (c) 2013 Michael Matan. All rights reserved.
+//
+
+#import "SOSBodyViewController.h"
+
+@interface SOSBodyViewController ()
+
+@end
+
+@implementation SOSBodyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
