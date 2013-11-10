be83f498b456b7c724cf39084f69a994b20fb637
simonbromberg <simon@encoretheapp.com>
2013-11-10 10:40:39 -0500

    added emergency view controller

---
+++ SOSBaycrest/SOSBaycrest/SOSEmergencyViewController.h
@@ -0,0 +1,12 @@
+//
+//  SOSEmergencyViewController.h
+//  SOSBaycrest
+//
+//  Created by Simon Bromberg on 11/10/2013.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SOSEmergencyViewController : UIViewController
+
+@end
---
+++ SOSBaycrest/SOSBaycrest/SOSEmergencyViewController.m
@@ -0,0 +1,12 @@
+//
+//  SOSEmergencyViewController.m
+//  SOSBaycrest
+//
+//  Created by Simon Bromberg on 11/10/2013.
+//
+
+#import "SOSEmergencyViewController.h"
+
+@implementation SOSEmergencyViewController
+
+@end
